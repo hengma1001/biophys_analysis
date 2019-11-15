99884e7b5f2037fac95ed429a00f33ac3928a4fe
Heng Ma <maheng1991@hotmail.com>
2019-11-15 14:22:05 -0600

    adapted for DeepDriveMD output

--- traj_to_cm.py
+++ traj_to_cm.py
@@ -11,23 +11,32 @@ from utils import triu_to_full
 
 print "Calculating contact maps from MD trajectories" 
 
-pdb_file = '../1FME-0-protein/1FME-0.pdb' 
-dcd_files = sorted(glob.glob('../1FME-0-protein/1FME-0-protein-*.dcd'))
+pdb_file = '../pdb/vhp1ww.pdb' 
+dcd_files = sorted(glob.glob('../omm_results/omm_runs_157*/output.dcd'))
 
 contact_maps = []
-mda_traj = mda.Universe(pdb_file, dcd_files)  
-protein_ca = mda_traj.select_atoms('protein and name CA') 
-		
-for _ in tqdm(mda_traj.trajectory): 
-	contact_map = triu_to_full(
-		(distances.self_distance_array(protein_ca.positions) < 8.0) * 1.0
-		)
-	contact_maps.append(contact_map) 
+for dcd_file in dcd_files: 
+    mda_traj = mda.Universe(pdb_file, dcd_file)  
+    protein_ca = mda_traj.select_atoms('protein and name CA') 
+                    
+    for _ in tqdm(mda_traj.trajectory): 
+        contact_map = triu_to_full(
+                (distances.self_distance_array(protein_ca.positions) < 8.0) * 1.0
+                )
+        contact_maps.append(contact_map) 
 
 contact_maps = np.array(contact_maps)
+
+# padding if odd dimension occurs in image
+pad_f = lambda x: (0,0) if x%2 == 0 else (0,1)
+padding_buffer = [(0,0)]
+for x in contact_maps.shape[1:]:
+    padding_buffer.append(pad_f(x))
+contact_maps = np.pad(contact_maps, padding_buffer, mode='constant')
+
 contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
 
-cm_h5 = h5py.File('contact_maps_%s.h5' % os.path.basename(pdb_file)[:-4], 'w') 
+cm_h5 = h5py.File('contact_maps_vhp.h5', 'w') 
 cm_h5.create_dataset('contact_maps', data=contact_maps) 
 cm_h5.close() 
 
