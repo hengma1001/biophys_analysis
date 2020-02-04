4f1ba8df7a79a1270d9f3a9e443c552287908c28
Heng Ma <heng.ma@anl.gov>
2020-02-04 19:25:57 +0000

    added generator module to utils

--- py_modules/utils.py
+++ py_modules/utils.py
@@ -19,13 +19,13 @@ def triu_to_full(cm0):
 
 
 def read_h5py_file(h5_file): 
-    cm_h5 = h5py.File(h5_file, 'r', libver='latest', swmr=True)
-    if 'contact_maps' in cm_h5.keys(): 
-        return cm_h5[u'contact_maps'] 
-    elif 'contacts' in cm_h5.keys(): 
-        return cm_h5['contacts'] 
-    else: 
-        return [] 
+    with h5py.File(h5_file, 'r', libver='latest', swmr=True) as cm_h5: 
+        if 'contact_maps' in cm_h5.keys(): 
+            return cm_h5[u'contact_maps'].value 
+        elif 'contacts' in cm_h5.keys(): 
+            return cm_h5['contacts'].value
+        else: 
+            return [] 
 
 
 def cm_to_cvae(cm_data_lists): 
@@ -38,6 +38,41 @@ def cm_to_cvae(cm_data_lists):
 
     # transfer upper triangle to full matrix 
     cm_data_full = np.array([triu_to_full(cm_data) for cm_data in cm_all.T]) 
+
+    # padding if odd dimension occurs in image 
+    pad_f = lambda x: (0,0) if x%2 == 0 else (0,1) 
+    padding_buffer = [(0,0)] 
+    for x in cm_data_full.shape[1:]: 
+        padding_buffer.append(pad_f(x))
+    cm_data_full = np.pad(cm_data_full, padding_buffer, mode='constant')
+
+    # reshape matrix to 4d tensor 
+    cvae_input = cm_data_full.reshape(cm_data_full.shape + (1,))   
+    
+    return cvae_input
+
+
+
+def trim_cm(contact_map, resnum_intersted): 
+    for i in np.arange(contact_map.shape[0]): 
+        if i not in resnum_intersted: 
+            contact_map[i] = 10 
+            contact_map[:,i] = 10 
+    contact_map = contact_map[contact_map != 10] 
+    assert len(contact_map) == len(resnum_intersted) ** 2
+    return contact_map.reshape(len(resnum_intersted), len(resnum_intersted))
+
+
+def cm_to_cvae_trim(cm_data_lists, resnum_intersted): 
+    """
+    A function converting the 2d upper triangle information of contact maps 
+    read from hdf5 file to full contact map of interested residues and 
+    reshape to the format ready for cvae
+    """
+    cm_all = np.hstack(cm_data_lists)
+
+    # transfer upper triangle to full matrix 
+    cm_data_full = np.array([trim_cm(triu_to_full(cm_data), resnum_intersted) for cm_data in cm_all.T]) 
 
     # padding if odd dimension occurs in image 
     pad_f = lambda x: (0,0) if x%2 == 0 else (0,1) 
@@ -102,3 +137,44 @@ def outliers_from_latent(cm_predict, eps=0.35):
     db_label = db.labels_
     outlier_list = np.where(db_label == -1)
     return outlier_list
+
+
+def read_h5py_len(h5_file):
+    with h5py.File(h5_file, 'r', libver='latest', swmr=True) as cm_h5:
+        if 'contact_maps' in cm_h5.keys():
+            return cm_h5[u'contact_maps'].shape[1]
+        elif 'contacts' in cm_h5.keys():
+            return cm_h5['contacts'].shape[1]
+        else:
+            return 0
+
+
+def cm_generator(cm_files, batch_size, resnum, shuffle=True): 
+#     samples_per_epoch = len(cm_files) 
+    number_of_files = len(cm_files)  
+    counter = 0 
+    if shuffle: 
+        np.random.shuffle(cm_files) 
+    cm_data_lists = []
+    
+    while 1: 
+        # assuming batch size is way smaller than number of cm in each file 
+        # therefore, it's sufficient to load only one extra while current is 
+        # exhausted 
+        if counter >= len(cm_files) and len(cm_data_lists) < batch_size: 
+            # Reset counter to next epoch, abandon last batch of frames here 
+            # cvae_input, cm_data_lists = cm_data_lists, [] 
+            counter = 0 
+        else: 
+            if len(cm_data_lists) < batch_size: 
+                # read a new h5 file when the leftover is smaller than batch size 
+                new_cm = cm_to_cvae_trim([read_h5py_file(cm_files[counter])], resnum) 
+#                 print(new_cm.shape) 
+                counter += 1 
+                if cm_data_lists == []: 
+                    cm_data_lists = new_cm
+                else: 
+                    cm_data_lists = np.array(np.vstack([cm_data_lists, new_cm])) 
+            cvae_input, cm_data_lists = cm_data_lists[:batch_size], cm_data_lists[batch_size:]
+        
+        yield cvae_input, cvae_input 
