c3a77f48760e238900bde2e306c7df52f8a8b05e
Heng Ma <maheng1991@hotmail.com>
2020-07-21 12:25:05 -0700

    new updates

--- traj_analysis/traj_to_cm.py
+++ traj_analysis/traj_to_cm.py
@@ -12,27 +12,27 @@ from utils import triu_to_full
 
 print("Calculating contact maps from MD trajectories") 
 
-pdb_files = sorted(glob.glob('../../protein_only/omm_runs_*/*.pdb')) 
-dcd_files = sorted(glob.glob('../../protein_only/omm_runs_*/*.dcd'))
+pdb_file = '../../md_runs/cpep.gro' 
+dcd_files = sorted(glob.glob('../../md_runs/rex_0*/output.dcd'))
 
 contact_maps = []
-labels = []
-label_kinds = set()
+# labels = []
+# label_kinds = set()
 # mda_traj = mda.Universe(pdb_file, dcd_files)  
 # protein_ca = mda_traj.select_atoms('protein and name CA') 
 		
-for pdb, dcd in tqdm(zip(pdb_files, dcd_files)): 
-    mda_traj = mda.Universe(pdb, dcd) 
+for dcd in tqdm(dcd_files): 
+    mda_traj = mda.Universe(pdb_file, dcd) 
     protein_ca = mda_traj.select_atoms('protein and name CA')
-    label = os.path.basename(os.path.dirname(pdb)).split('_')[2]
-    label_kinds.add(label) 
+#     label = os.path.basename(os.path.dirname(pdb)).split('_')[2]
+#     label_kinds.add(label) 
 
     for _ in mda_traj.trajectory: 
             contact_map = triu_to_full(
                     (distances.self_distance_array(protein_ca.positions) < 8.0) * 1.0
                     )
             contact_maps.append(contact_map) 
-            labels.append(len(label_kinds)-1) 
+#             labels.append(len(label_kinds)-1) 
 
 contact_maps = np.array(contact_maps)
 
@@ -48,7 +48,7 @@ contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
 
 cm_h5 = h5py.File('contact_maps.h5', 'w') 
 cm_h5.create_dataset('contact_maps', data=contact_maps) 
-cm_h5.create_dataset('system', data=labels) 
+# cm_h5.create_dataset('system', data=labels) 
 cm_h5.close() 
 
 print('Done')
