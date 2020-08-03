fixed cvae bug and updated cm calculation

traj_analysis/traj_to_cm.py
@@ -12,28 +12,37 @@ from utils import triu_to_full
 
 print("Calculating contact maps from MD trajectories") 
 
-pdb_file = '../../md_runs/cpep.gro' 
-dcd_files = sorted(glob.glob('../../md_runs/rex_0*/output.dcd'))
+# pdb_file = '../../md_runs/cpep.gro' 
+# dcd_files = sorted(glob.glob('../../md_runs/rex_0*/output.dcd'))
+pdb_files = sorted(glob.glob('../../Deepdrive_mds/omm_runs_*/comp.pdb'))
+dcd_files = sorted(glob.glob('../../Deepdrive_mds/omm_runs_*/output.dcd'))
 
 contact_maps = []
+failed = [] 
 # labels = []
 # label_kinds = set()
 # mda_traj = mda.Universe(pdb_file, dcd_files)  
 # protein_ca = mda_traj.select_atoms('protein and name CA') 
 		
-for dcd in tqdm(dcd_files): 
-    mda_traj = mda.Universe(pdb_file, dcd) 
+for pdb_file, traj_file in tqdm(zip(pdb_files, dcd_files)): 
+#     mda_traj = mda.Universe(pdb_file, dcd) 
+    try: 
+        mda_traj = mda.Universe(pdb_file, traj_file) 
+    except OSError:
+        failed += [pdb_file] 
+        continue
     protein_ca = mda_traj.select_atoms('protein and name CA')
 #     label = os.path.basename(os.path.dirname(pdb)).split('_')[2]
 #     label_kinds.add(label) 
 
-    for _ in mda_traj.trajectory: 
+    for _ in mda_traj.trajectory[::10]: 
             contact_map = triu_to_full(
-                    (distances.self_distance_array(protein_ca.positions) < 8.0) * 1.0
+                    (distances.self_distance_array(protein_ca.positions) < 8.0) * 1
                     )
             contact_maps.append(contact_map) 
 #             labels.append(len(label_kinds)-1) 
 
+print("failed MD cases: ", failed) 
 contact_maps = np.array(contact_maps)
 
 # padding if odd dimension occurs in image
@@ -43,6 +52,7 @@ padding_buffer = [(0,0)]
 for x in contact_maps.shape[1:]:
     padding_buffer.append(pad_f(x))
 contact_maps = np.pad(contact_maps, padding_buffer, mode='constant')
+print(contact_maps.shape) 
 
 contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
 

traj_analysis/traj_to_cm_sel.py
@@ -13,30 +13,29 @@ from utils import triu_to_full
 
 print("Calculating contact maps from MD trajectories") 
 
-pdb_files = sorted(glob.glob('../../MD_trajs/*_run_*/*.pdb')) 
-dcd_files = sorted(glob.glob('../../MD_trajs/*_run_*/*.dcd'))
+pdb_files = sorted(glob.glob('../../Deepdrive_mds/omm_runs_*/comp.pdb'))
+dcd_files = sorted(glob.glob('../../Deepdrive_mds/omm_runs_*/output.dcd'))
+np_file = './mpro_res.npy' 
 
 contact_maps = []
-labels = []
-label_kinds = set()
+failed = []
 # mda_traj = mda.Universe(pdb_file, dcd_files)  
 # protein_ca = mda_traj.select_atoms('protein and name CA') 
 		
 for pdb, dcd in tqdm(zip(pdb_files, dcd_files)): 
-    mda_traj = mda.Universe(pdb, dcd) 
+    try: 
+        mda_traj = mda.Universe(pdb, dcd) 
+    except OSError: 
+        failed += [pdb] 
+        continue 
     protein_ca = mda_traj.select_atoms('protein and name CA')
-    label = os.path.basename(os.path.dirname(pdb)).split('_')[1]
-    label_kinds.add(label) 
-    json_file = '../../sel_resnum/%s_input.json' % label 
-    res_dict = json.load(open(json_file, 'r'))
-    resl_1 = res_dict['x']
-    resl_2 = res_dict['y'] 
+    resl_1 = np.load(np_file) 
 
     for _ in mda_traj.trajectory: 
-            contact_map = (distances.distance_array(protein_ca.positions[resl_1], protein_ca.positions[resl_2]) < 8.0) * 1.0
+            contact_map = (distances.distance_array(protein_ca.positions[resl_1], protein_ca.positions) < 8.0) * 1.0
             contact_maps.append(contact_map) 
-            labels.append(len(label_kinds)-1) 
 
+print("failed MD on ", failed) 
 contact_maps = np.array(contact_maps)
 
 # padding if odd dimension occurs in image
@@ -51,7 +50,6 @@ contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
 
 cm_h5 = h5py.File('contact_maps.h5', 'w') 
 cm_h5.create_dataset('contact_maps', data=contact_maps) 
-cm_h5.create_dataset('system', data=labels) 
 cm_h5.close() 
 
 print('Done')