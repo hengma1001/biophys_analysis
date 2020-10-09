5f0e6bcc47495867bb14494fab16bb44bf17e91e
Heng Ma <maheng1991@hotmail.com>
2020-10-08 19:52:37 -0700

    updated for nsp10/16 multiple systems

--- traj_analysis/traj_to_cm.py
+++ traj_analysis/traj_to_cm.py
@@ -7,49 +7,88 @@ from tqdm import tqdm
 
 import MDAnalysis as mda 
 from MDAnalysis.analysis import distances 
+from MDAnalysis.analysis.rms import RMSD
 sys.path.append("../py_modules") 
 from utils import triu_to_full
 
 print("Calculating contact maps from MD trajectories") 
 
-pdb_file = '../../md_runs/cpep.gro' 
-dcd_files = sorted(glob.glob('../../md_runs/rex_0*/output.dcd'))
+runs = glob.glob("../../run_*")
 
-contact_maps = []
-# labels = []
-# label_kinds = set()
-# mda_traj = mda.Universe(pdb_file, dcd_files)  
-# protein_ca = mda_traj.select_atoms('protein and name CA') 
-		
-for dcd in tqdm(dcd_files): 
-    mda_traj = mda.Universe(pdb_file, dcd) 
+h5_output = "h5_output" 
+os.makedirs(h5_output, exist_ok=True) 
+
+traj_out = 'traj_out'
+os.makedirs(traj_out, exist_ok=True)
+
+for run in runs: 
+    label = os.path.basename(run)[4:]
+    print(f"Running system {label}...") 
+    pdb_file = run + f"/{label}.pdb" 
+    traj_file = run + "/output.dcd"
+    mda_traj = mda.Universe(pdb_file, traj_file) 
+
+    # prepare trajecory
+    pdb_out = traj_out + f'/{label}_no_sol.pdb'
+    dcd_out = traj_out + f'/{label}_no_sol.dcd'
+    no_sol = mda_traj.select_atoms('not resname SOL and not resname NA and not resname CL')
+    no_sol.write(pdb_out)
+    if label.startswith("comp"):
+        nsp10 = mda_traj.segments[1].atoms 
+        nsp16 = mda_traj.segments[0].atoms 
+        with mda.Writer(dcd_out, no_sol.n_atoms) as W: 
+            for ts in tqdm(mda_traj.trajectory): 
+                box_edge = ts.dimensions[0]
+                box_center = box_edge / 2
+                trans_vec = box_center - np.array(nsp16.center_of_mass())
+                no_sol.atoms.translate(trans_vec).wrap()
+                trans_vec = box_center - np.array(no_sol.center_of_mass())
+                no_sol.atoms.translate(trans_vec).wrap()
+                W.write(no_sol)
+    else: 
+        with mda.Writer(dcd_out, no_sol.n_atoms) as W:
+            for ts in tqdm(mda_traj.trajectory):
+                W.write(no_sol)
+    mda_traj = mda.Universe(pdb_out, dcd_out)
+
+    # prep ref traj 
+    if label.startswith("comp"):
+        ref_pdb = "../../run_comp/comp.pdb"
+    else:
+        ref_pdb = pdb_file
+    ref_traj = mda.Universe(ref_pdb) 
+
+    rmsd_inst = RMSD(mda_traj, ref_traj, 
+            select='protein and name CA',
+            verbose=1) 
+    rmsd_inst.run() 
+    rmsd_profs = rmsd_inst.rmsd 
+    
     protein_ca = mda_traj.select_atoms('protein and name CA')
-#     label = os.path.basename(os.path.dirname(pdb)).split('_')[2]
-#     label_kinds.add(label) 
-
-    for _ in mda_traj.trajectory: 
+    contact_maps = []
+    for _ in tqdm(mda_traj.trajectory): 
             contact_map = triu_to_full(
-                    (distances.self_distance_array(protein_ca.positions) < 8.0) * 1.0
+                    (distances.self_distance_array(protein_ca.positions) < 8.0) * 1
                     )
             contact_maps.append(contact_map) 
-#             labels.append(len(label_kinds)-1) 
+    contact_maps = np.array(contact_maps)
 
-contact_maps = np.array(contact_maps)
+    # padding if odd dimension occurs in image
+    padding = 4 
+    pad_f = lambda x: (0,0) if x%padding == 0 else (0,padding-x%padding)
+    padding_buffer = [(0,0)]
+    for x in contact_maps.shape[1:]:
+        padding_buffer.append(pad_f(x))
+    contact_maps = np.pad(contact_maps, padding_buffer, mode='constant')
+    print(contact_maps.shape) 
 
-# padding if odd dimension occurs in image
-padding = 4 
-pad_f = lambda x: (0,0) if x%padding == 0 else (0,padding-x%padding)
-padding_buffer = [(0,0)]
-for x in contact_maps.shape[1:]:
-    padding_buffer.append(pad_f(x))
-contact_maps = np.pad(contact_maps, padding_buffer, mode='constant')
+    contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
 
-contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
-
-cm_h5 = h5py.File('contact_maps.h5', 'w') 
-cm_h5.create_dataset('contact_maps', data=contact_maps) 
-# cm_h5.create_dataset('system', data=labels) 
-cm_h5.close() 
+    h5_file = h5_output + f"/cm_{label}.h5"
+    cm_h5 = h5py.File(h5_file, 'w') 
+    cm_h5.create_dataset('contact_maps', data=contact_maps) 
+    cm_h5.create_dataset('rmsd', data=rmsd_profs) 
+    cm_h5.close() 
 
 print('Done')
 # print dcd_files 
