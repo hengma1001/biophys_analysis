230678b7112c5d425dbb7e2c56c9cfdf4a965082
Heng Ma <maheng1991@hotmail.com>
2020-11-30 13:57:50 -0800

    modified distance module

--- traj_analysis/traj_to_cm.py
+++ traj_analysis/traj_to_cm.py
@@ -13,7 +13,7 @@ from utils import triu_to_full
 
 print("Calculating contact maps from MD trajectories") 
 
-runs = glob.glob("../../run_*")
+runs = glob.glob("../../md_runs/run_*")
 
 h5_output = "h5_output" 
 os.makedirs(h5_output, exist_ok=True) 
@@ -21,10 +21,12 @@ os.makedirs(h5_output, exist_ok=True)
 traj_out = 'traj_out'
 os.makedirs(traj_out, exist_ok=True)
 
+contact_maps = []
+rmsd_profs = [] 
 for run in runs: 
     label = os.path.basename(run)[4:]
     print(f"Running system {label}...") 
-    pdb_file = run + f"/{label}.pdb" 
+    pdb_file = run + f"/comp.pdb" 
     traj_file = run + "/output.dcd"
     mda_traj = mda.Universe(pdb_file, traj_file) 
 
@@ -53,7 +55,7 @@ for run in runs:
 
     # prep ref traj 
     if label.startswith("comp"):
-        ref_pdb = "../../run_comp/comp.pdb"
+        ref_pdb = "../../md_runs/run_comp_000/comp.pdb"
     else:
         ref_pdb = pdb_file
     ref_traj = mda.Universe(ref_pdb) 
@@ -62,33 +64,33 @@ for run in runs:
             select='protein and name CA',
             verbose=1) 
     rmsd_inst.run() 
-    rmsd_profs = rmsd_inst.rmsd 
+    rmsd_profs.append(rmsd_inst.rmsd)
     
     protein_ca = mda_traj.select_atoms('protein and name CA')
-    contact_maps = []
     for _ in tqdm(mda_traj.trajectory): 
             contact_map = triu_to_full(
                     (distances.self_distance_array(protein_ca.positions) < 8.0) * 1
                     )
             contact_maps.append(contact_map) 
-    contact_maps = np.array(contact_maps)
+contact_maps = np.array(contact_maps)
+rmsd_profs = np.array(rmsd_profs) 
 
-    # padding if odd dimension occurs in image
-    padding = 4 
-    pad_f = lambda x: (0,0) if x%padding == 0 else (0,padding-x%padding)
-    padding_buffer = [(0,0)]
-    for x in contact_maps.shape[1:]:
-        padding_buffer.append(pad_f(x))
-    contact_maps = np.pad(contact_maps, padding_buffer, mode='constant')
-    print(contact_maps.shape) 
+# padding if odd dimension occurs in image
+padding = 4 
+pad_f = lambda x: (0,0) if x%padding == 0 else (0,padding-x%padding)
+padding_buffer = [(0,0)]
+for x in contact_maps.shape[1:]:
+    padding_buffer.append(pad_f(x))
+contact_maps = np.pad(contact_maps, padding_buffer, mode='constant')
+print(contact_maps.shape) 
 
-    contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
+contact_maps = contact_maps.reshape((contact_maps.shape) + (1,))
 
-    h5_file = h5_output + f"/cm_{label}.h5"
-    cm_h5 = h5py.File(h5_file, 'w') 
-    cm_h5.create_dataset('contact_maps', data=contact_maps) 
-    cm_h5.create_dataset('rmsd', data=rmsd_profs) 
-    cm_h5.close() 
+h5_file = h5_output + f"/cm_{label}.h5"
+cm_h5 = h5py.File(h5_file, 'w') 
+cm_h5.create_dataset('contact_maps', data=contact_maps) 
+cm_h5.create_dataset('rmsd', data=rmsd_profs) 
+cm_h5.close() 
 
 print('Done')
 # print dcd_files 
