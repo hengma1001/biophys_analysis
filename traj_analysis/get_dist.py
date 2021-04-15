1ecf210e1a9a83f42ba0ab11cb0bcb510c25b85e
Heng Ma <maheng1991@hotmail.com>
2021-04-15 13:53:17 -0700

    updated distance calculation script.

--- traj_analysis/get_dist.py
+++ traj_analysis/get_dist.py
@@ -1,66 +1,57 @@
-import os
-import sys 
-import glob 
-import h5py 
+import os 
+import glob
 import numpy as np 
-from tqdm import tqdm 
-
-import MDAnalysis as mda 
+import pandas as pd 
+import MDAnalysis as mda
+from tqdm import tqdm
 from numpy import linalg as LA
-sys.path.append("../py_modules") 
-from utils import triu_to_full
 
-def dist_pbc(a, b, box):
+def dist_pbc(a, b, box): 
     """
-    calculate distance between two points
+    calculate distance between two points 
     in PBC box
     """
-    assert len(a) == len(b)
+    assert len(a) == len(b) 
     box = box[:len(a)]
-    a = a % box
+    a = a % box 
     b = b % box
     dist_vec = np.abs(a - b)
     dist_vec = np.abs(dist_vec - box * (dist_vec > box/2))
-    return LA.norm(dist_vec)
-    print(dist_vec)
+    return LA.norm(dist_vec) 
+    print(dist_vec) 
+    
 
-print("Calculating distance between complexes from MD trajectories") 
+# a = np.array([1., 2.6]); b = np.array([3.6, 2])
+# box = np.array([4., 4.]); c = np.array([10., 9.])
+# dist_pbc(a, b, box)
+# dist_pbc(a, c, box)
 
-# pdb_file = '../../md_runs/cpep.gro' 
-# dcd_files = sorted(glob.glob('../../md_runs/rex_0*/output.dcd'))
-pdb_files = sorted(glob.glob('/lus/theta-fs0/projects/RL-fold/msalim/production-runs/pasc/nsp1016_448_gpu.1/md_runs/run*_*/*.pdb'))
-dcd_files = sorted(glob.glob('/lus/theta-fs0/projects/RL-fold/msalim/production-runs/pasc/nsp1016_448_gpu.1/md_runs/run*_*/*.dcd'))
+# exit()
+runs = glob.glob("./nsp10_16_*") 
 
-assert len(pdb_files) == len(dcd_files) 
-dists = []
-# labels = []
-# label_kinds = set()
-# mda_traj = mda.Universe(pdb_file, dcd_files)  
-# protein_ca = mda_traj.select_atoms('protein and name CA') 
-		
-for pdb_file in tqdm(pdb_files): 
-    traj_file = dcd_files[pdb_files.index(pdb_file)]
-#     mda_traj = mda.Universe(pdb_file, dcd) 
-    try: 
-        mda_traj = mda.Universe(pdb_file, traj_file) 
-    except OSError:
-        print(pdb_file) 
-        continue
-    protein_ca = mda_traj.select_atoms('protein and name CA')
-    nsp16 = mda_traj.segments[0].atoms
-    nsp10 = mda_traj.segments[1].atoms
-#     label = os.path.basename(os.path.dirname(pdb)).split('_')[2]
-#     label_kinds.add(label) 
+dist_collect = [] 
+for run in runs: 
+    pdbs = glob.glob(f"{run}/*pdb") 
+    for pdb in tqdm(pdbs):
+        sys_name = os.path.basename(pdb)[:-4]
+        dcd = pdb.replace('pdb', 'dcd') 
 
-    for _ in mda_traj.trajectory[::5]: 
-        dist = dist_pbc(
-                nsp10.center_of_mass(), 
-                nsp16.center_of_mass(), 
-                nsp16.dimensions) 
-        dists += [dist]
+        mda_traj = mda.Universe(pdb, dcd) 
+        nsp16 = mda_traj.segments[0].atoms
+        nsp10 = mda_traj.segments[1].atoms
 
-dists = np.array(dists)
+        dist_list = []
+        for ts in mda_traj.trajectory: 
+            box = ts.dimensions 
+            nsp10_c = nsp10.center_of_mass() 
+            nsp16_c = nsp16.center_of_mass() 
+            dist_list += [dist_pbc(nsp10_c, nsp16_c, box)] 
+        dist_collect.append({"run": run, "sys_name": sys_name, 
+                "dist": dist_list}) 
 
-np.save('dist.npy', dists)
-print('Done')
-# print dcd_files 
+df = pd.DataFrame(dist_collect) 
+df.to_pickle('nsp10_16_dist.pkl') 
+        
+
+
+
